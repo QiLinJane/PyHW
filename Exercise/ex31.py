fcceb8c7d66caeb3f1114bc7158d06d82a3279db
wangjane <lxy05021030@163.com>
2018-10-10 23:11:14 +0800

    ex31

---
+++ Exercise/ex31.py
@@ -0,0 +1,33 @@
+# coding=utf-8
+
+print "you enter a dark room with two doors. do you go through door #1 or door #2?"
+
+door = raw_input(">")
+if door == "1":
+	print "there is  a giant bear here eating a cheese cake. what do you do?"
+	print "1. Take the cake."
+	print "2. scream at the bear."
+
+	bear = raw_input("> ")
+
+	if bear == "1":
+		print "the bear eats your face off. good job!"
+	elif bear == "2":
+		print "the bear eats your legs off. good job!"
+	else:
+		print "well, doing %s is probably better. bear runs away." % bear
+
+elif door == "2":
+	print "you stare into the endless abyss at cthulhu's retina."
+	print "1. blueberris."
+	print "2. yellow jacket clothespins."
+	print "3. understanding revolvers yelling melodis."
+
+	insanity = raw_input("> ")
+
+	if insanity == "1" or  insanity == "2":
+		print "your body survives powered by a mind of jello. good job!"
+	else:
+		print "the insanity rots your eyes into a pool of muck. good job!"
+else:
+	print "you stumble around and fall on a knife and  die. good job"
